daaddc4e2010a82d1b6e60348ba29a58c1622ac1
AndresManrique <AndresManrique@Manrique>
2014-12-11 23:06:47 -0500

    consultarEstado

---
+++ EsquemaFinal/vistas/registroCorrecto.php
@@ -0,0 +1,28 @@
+<?php include('header.php'); ?>
+	<?php include('BarraNavegacion.php'); ?>
+    <div class="container">
+		<div class="margin-top">
+			<div class="row">
+					<?php include('head.php'); ?>
+				
+			
+                            <br><br>
+					<div class="text_content">
+					<div class="controls" align="center">
+                                            <!-- Contenido Texto --><br>
+                       <label class="control-label"> <h2> Usuario Registrado </h2></label>
+                                                  <br>
+                                        <IMG SRC="bien.jpg"  width='150' height='150' /><br>
+                                      	</div>
+					</div>
+					<!-- end content -->
+				
+				
+				</div>
+			
+			</div>
+		</div>
+	
+		
+    </div>
+<?php include('footer.php') ?>
