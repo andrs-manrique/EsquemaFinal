863b4163d4e83a5b472b28fb9935f9d34c946508
AndresManrique <AndresManrique@192.168.17.208>
2014-12-13 18:34:42 -0500

    Modificar Colaborador; y

--- EsquemaFinal/vistas/editarDatosColaborador.php
+++ EsquemaFinal/vistas/editarDatosColaborador.php
@@ -2,13 +2,9 @@
 <?php include("header.php");?>
 <?php include("../3modelo/autenticacion.php");?>
 <?php include("BarraNavegacionadmin.php");?>	
-<div aling="center">
-
-				  <?php 
-                                  
-				  	include("../3modelo/conexion.php");
-
-				    $identificacion = $_SESSION['identificacion'];
+  <?php 
+ 	include("../3modelo/conexion.php");
+    $identificacion = $_SESSION['identificacion'];
 $id_col = $_GET['dni'];
 $consulta=mysql_query("select *from t_usuario where (dni_user='$id_col')");		
 if($row=mysql_fetch_array($consulta))
@@ -40,55 +36,52 @@ $id_jornada=$row["id_jornada"];
 $rol_user=$row["rol_user"];
 $id_materia_user=$row["id_materia_user"];
 }
-				  ?>
-				  <div>
-	<font color="white">     <h3>DATOS PERSONALES</h3>
-          <p>Modificar Datos COLABORADR</p></font>
+?>
+<div aling="center">
+  <center> 
+<h3>DATOS PERSONALES</h3>
+<p>Modificar Datos COLABORADOR</p>
+    </center></font>
 <form name="form" method="post" action="../2controlador/C_Usuario.php" > 
  <table align="center">
   <tr>
-    <td class="registr">Identificación</td>
+    <td class="registr">No. Documento:</td>
     <td class="registr"><input type="text" name="dni" title="usuario" readonly="readonly" value='<?php echo $_GET['dni']; ?>'></td>
   
-    <td class="registr">Tipo DNi</td>
+    <td class="registr">Tipo de Documento:</td>
     <td class="registr"><input type="text" name="tipodni_user" title="usuario" readonly="readonly" value='<?php echo $tipodni_user; ?>'></td>
   </tr>
-  <td>
-   <td class="registr"></td>
-    <td class="registr"><input type="hidden" name="foto_user" title="usuario" readonly="readonly" value='<?php echo $tipodni_user; ?>'></td>
-  
-  </tr>
-  
   <tr>
-    <td class="registr">Nombre</td>
+    <td class="registr"></td>
+    <td class="registr">
+   <input type="hidden" name="foto_user" title="usuario" readonly="readonly" value='<?php echo $foto_user; ?>'>
+    </td>
+  </tr>
+  <tr>
+    <td class="registr">Nombre(s)</td>
     <td class="registr"><input type="text" name="nom_user" title="nombre" value='<?php echo $nom_user; ?>'></td>
  
-	 <td class="registr">Apellido</td>
-	 <td class="registr"><input type="text" name="apell_user" title="apellido" value='<?php echo $apell_user;?>'></td>
-	</tr>
-        
-         <tr>
-	 <td class="registr">Sexo</td>
-	 <td class="registr"><input type="text" name="sex_user" title="apellido" value='<?php echo $sex_user;?>'></td>
-	</tr>
-	<tr>
-	 <td class="registr">Correo</td>
-	 <td class="registr"><input type="text" name="mail_user" title="correo" value='<?php echo $mail_user;?>'></td>
-        </tr>
+    <td class="registr">Apellido(s)</td>
+    <td class="registr"><input type="text" name="apell_user" title="apellido" value='<?php echo $apell_user;?>'></td>
+  </tr>      
+  <tr>
+   <td class="registr">Sexo</td>
+   <td class="registr"><input type="text" name="sex_user" title="apellido" value='<?php echo $sex_user;?>'></td>
+   <td class="registr">Correo</td>
+   <td class="registr"><input type="text" name="mail_user" title="correo" value='<?php echo $mail_user;?>'></td>
+  </tr>
         <tr>
-	 <td class="registr">clave</td>
-	 <td class="registr"><input type="text" name="clave_user" title="nac_user" value='<?php echo $clave_user;?>'></td>
+	 <td class="registr"></td>
+	 <td class="registr"><input type="hidden" name="clave_user" title="nac_user" value='<?php echo $clave_user;?>'></td>
 	</tr>      
         
         <tr>
-    <td class="registr">Telefono Usuario</td>
+    <td class="registr">Telefono </td>
     <td class="registr"><input type="text" name="tel_user" title="nombre" value='<?php echo $tel_user; ?>'></td>
  
 	 <td class="registr">Estado Civil</td>
 	 <td class="registr"><input type="text" name="civil_user" title="apellido" value='<?php echo $civil_user;?>'></td>
-	</tr>
-        
-        
+	</tr> 
     <tr>
      <td class="registr">Lugar Nacimiento:</td>
      <td class="registr"><input type="text" name="lugarnac_user" title="clave_user" readonly="readonly" value='<?php echo $lugarnac_user;?>'></td>
@@ -98,30 +91,30 @@ $id_materia_user=$row["id_materia_user"];
     </tr>
 
      <tr>
-     <td class="registr">localidad:</td>
-     <td class="registr"><input type="text" name="localidad_user" title="localidad vive" readonly="readonly" value='<?php echo $localidad_user;?>'></td>
+     <td class="registr"></td>
+     <td class="registr"><input type="hidden" name="localidad_user" title="localidad vive" readonly="readonly" value='<?php echo $localidad_user;?>'></td>
     
-     <td class="registr">estrato:</td>
-     <td class="registr"><input type="text" name="estrato_user" title="estrato de vivenci" readonly="readonly" value='<?php echo $estrato_user;?>'></td>
+     <td class="registr"></td>
+     <td class="registr"><input type="hidden" name="estrato_user" title="estrato de vivenci" readonly="readonly" value='<?php echo $estrato_user;?>'></td>
     
-     <td class="registr">barrio:</td>
-     <td class="registr"><input type="text" name="barrio_user" title="barrio" readonly="readonly" value='<?php echo $barrio_user;?>'></td>
+     <td class="registr"></td>
+     <td class="registr"><input type="hidden" name="barrio_user" title="barrio" readonly="readonly" value='<?php echo $barrio_user;?>'></td>
     
      </tr>
 
      <tr>
          
-     <td class="registr">Archivo De registro:</td>
-     <td class="registr"><input type="text" name="archivo_usuario" title="localidad vive" readonly="readonly" value='<?php echo $archivo_usuario;?>'></td>
+     <td class="registr"></td>
+     <td class="registr"><input type="hidden" name="archivo_usuario" title="localidad vive" readonly="readonly" value='<?php echo $archivo_usuario;?>'></td>
     
      </tr>
     <tr>
          
-     <td class="registr">Acudiente</td>
-     <td class="registr"><input type="text" name="responsable_user" title="localidad vive" readonly="readonly" value='<?php echo $responsable_user;?>'></td>
+     <td class="registr"></td>
+     <td class="registr"><input type="hidden" name="responsable_user" title="localidad vive" readonly="readonly" value='<?php echo $responsable_user;?>'></td>
      
-     <td class="registr">Cel acudiente</td>
-     <td class="registr"><input type="text" name="cel_respon_user" title="localidad vive" readonly="readonly" value='<?php echo $cel_respon_user;?>'></td>
+     <td class="registr"></td>
+     <td class="registr"><input type="hidden" name="cel_respon_user" title="localidad vive" readonly="readonly" value='<?php echo $cel_respon_user;?>'></td>
     
      </tr>
     
@@ -129,23 +122,23 @@ $id_materia_user=$row["id_materia_user"];
          
      <td class="registr">Fecha Registro</td>
      <td class="registr"><input type="text" name="fecha_registro_user" title="localidad vive" readonly="readonly" value='<?php echo $fecha_registro_user;?>'></td>
-     
-     <td class="registr">carrera_user</td>
+    </tr>
+    <tr>
+     <td class="registr">Pregrado:</td>
      <td class="registr"><input type="text" name="carrera_user" title="localidad vive" readonly="readonly" value='<?php echo $carrera_user;?>'></td>
-    <td class="registr">Universidad </td>
+    <td class="registr">Universidad:</td>
      <td class="registr"><input type="text" name="universidad_user" title="localidad vive" readonly="readonly" value='<?php echo $universidad_user;?>'></td>
-   
      
      </tr>
      <tr>
-         
-     <td class="registr">Tipo Colegio</td>
-     <td class="registr"><input type="text" name="id_colegio" title="localidad vive" readonly="readonly" value='<?php echo $id_colegio;?>'></td>
-     
      <td class="registr">Estado Usuario</td>
      <td class="registr"><input type="text" name="id_estado" title="localidad vive" readonly="readonly" value='<?php echo $id_estado;?>'></td>
-    <td class="registr">Jornada </td>
-     <td class="registr"><input type="text" name="id_jornada" title="localidad vive" readonly="readonly" value='<?php echo $id_jornada;?>'></td>
+     
+     <td class="registr"></td>
+     <td class="registr"><input type="hidden" name="id_colegio" title="localidad vive" readonly="readonly" value='<?php echo $id_colegio;?>'></td>
+     
+     <td class="registr"> </td>
+     <td class="registr"><input type="hidden" name="id_jornada" title="localidad vive" readonly="readonly" value='<?php echo $id_jornada;?>'></td>
      </tr>
          
          
@@ -157,12 +150,11 @@ $id_materia_user=$row["id_materia_user"];
    
      
      </tr>
-    
-   
+     <tr>
   <script language="JavaScript">function actualizar(){ document.form.txtoperacion.value = 'actualizar';}</script>    
     <td colspan="2" align="center" class="registr"> <input type="submit" name="Submit" value="Modificar Datos" onClick="actualizar()" >
     <input type='hidden' name='txtoperacion' value='des'>
-    </td></td>
+    </td>
     </tr>
 </table>
 </form> 					
