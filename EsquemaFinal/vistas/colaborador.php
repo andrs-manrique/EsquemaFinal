4534ab2624b0c7fdf0d771cdeddfa75966aab0d8
AndresManrique <AndresManrique@Manrique>
2014-12-25 04:30:09 -0500

    Inicio de Aspectos del Colaborador

--- EsquemaFinal/vistas/colaborador.php
+++ EsquemaFinal/vistas/colaborador.php
@@ -1,7 +1,9 @@
 <?php include('header.php'); ?>
-<?php include("BarraNavegacionadmin.php");?>			
+<?php include("BarrNavCol.php");?>			
 <?php include("../3modelo/autenticacion.php");?>
-		<div>
+<div  align="center" >
+		
+<div>
 				
 				  <ul>
                                       <font color="white">  <li><a>COLABORADOR</li> </font>
@@ -11,19 +13,19 @@
 $identificacion = $_SESSION['identificacion'];
                              
 		$consulta=mysql_query("select * from t_usuario where dni_user='$identificacion'");
-                
-						while($fila=mysql_fetch_array($consulta))
-						{
-                                                   
-							echo "<h1> <font color='white'>Bienvenido:  ".$fila['nom_user']." ".$fila['apell_user']."</font></h1>";
-						}
+           
+             while ($fila = mysql_fetch_array($consulta)) {
+            echo "<h1> <font color='white'>Bienvenido:  " . $fila['nom_user'] . " " . $fila['apell_user'] . "</font></h1>";
+            echo "<img src='../fotos/" . $fila['foto_user'] . "' width=150px></td>";
+        }
 				   ?>
 				   </a></li>
-				   <li><a href="datosAdministrador.php">Datos personales</a></li>
+				   <li><a href="datosColaborador.php">Datos personales</a></li>
 				   <li><a href="cambiarContrasena.php">Cambiar Contraseña</a></li>
 				  </ul>
 				 
-				</div><br>
+				</div>
+</div>
                                 		<?php include("footer.php");?>		
 
 	
