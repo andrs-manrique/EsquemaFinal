af11e6ff9bbd840eb75628da5c2a0d8ba01cb21a
AndresManrique <AndresManrique@Manrique>
2014-12-25 03:17:48 -0500

    Modelo de Novedad

--- EsquemaFinal/3modelo/M_novedad.php
+++ EsquemaFinal/3modelo/M_novedad.php
@@ -1,8 +1,24 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include ("conexion.php");
+
+class novedad {
+
+    private $id_novedad;
+    private $fecha_novedad;
+    private $titulo_novedad;
+    private $imagen_novedad;
+    private $desc_novedad;
+    private $contenido_novedad;
+    
+    function __construct($id_novedad, $fecha_novedad, $titulo_novedad, $imagen_novedad, $desc_novedad, $contenido_novedad) {
+        $this->id_novedad = $id_novedad;
+        $this->fecha_novedad = $fecha_novedad;
+        $this->titulo_novedad = $titulo_novedad;
+        $this->imagen_novedad = $imagen_novedad;
+        $this->desc_novedad = $desc_novedad;
+        $this->contenido_novedad = $contenido_novedad;
+    }
+    
+}
 
