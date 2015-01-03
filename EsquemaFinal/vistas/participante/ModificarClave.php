e098e86ed1709f33958b2c843c4de4c0927e5829
AndresManrique <AndresManrique@Manrique>
2015-01-03 17:09:47 -0500

    ClaveIntento Modf

---
+++ EsquemaFinal/vistas/participante/ModificarClave.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<?php include('header.php'); ?>
+<?php include("./BarrNavAsp.php.php");?>
+        <?php include("../../3modelo/autenticacion.php"); ?>
+        <div id="contenedor">
+            Contenido
+           
+            </div>
+            <?php include("../footer.php"); ?>		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
