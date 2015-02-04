df9a8c2c466465b36f9f3fe3e071b066bbd86372
medina1991 <manumedina19@hotmail.com>
2015-02-04 10:36:15 -0500

    Update M_Usuario.php

--- EsquemaFinal/3modelo/M_Usuario.php
+++ EsquemaFinal/3modelo/M_Usuario.php
@@ -301,7 +301,7 @@ class usuario {
     public function modificarParADMIN() { //                                                                                                                                                                                                                                      usuario_nombre='$this->nombre',usuario_apellido='$this->apellido',usuario_correo='$this->email',
         $consulta = "update t_usuario set dni_user='$this->dni',tipodni_user='$this->tipodocumento',nom_user='$this->nom_user',apell_user='$this->apell_user',sex_user='$this->sex_user',mail_user='$this->mail_user',tel_user='$this->tel_user',civil_user='$this->civil_user',lugarnac_user='$this->lugarnac_user',nac_user='$this->nac_user',localidad_user='$this->localidad_user',estrato_user='$this->estrato_user',barrio_user='$this->barrio_user',archivo_usuario='$this->archivo_usuario',responsable_user='$this->responsable_user',cel_respon_user='$this->cel_respon_user',foto_user='$this->foto_user',fecha_registro_user='$this->fecha_registro_user',carrera_user='$this->carrera_user',universidad_user='$this->universidad_user',id_colegio='$this->id_colegio',id_estado='$this->id_estado',id_jornada='$this->id_jornada',rol_user='$this->rol_user',id_materia_user='$this->id_materia_user',clasificacion_usuario='$this->clasificacion_usuario' where(dni_user='$this->dni')";
         if (mysql_query($consulta)) {
-            echo"<script language='javascript'> alert('La Actualizacion es un EXITO');  </script>";
+            echo"<script language='javascript'> alert(' Actualizacion Exitosa');  </script>";
 
             echo"<script language='javascript'>location.href=\"../vistas/listarAspirante.php\"   </script>";
         } else {
@@ -316,7 +316,7 @@ class usuario {
     public function actualizarCol() { //                                                                                                                                                                                                                                      usuario_nombre='$this->nombre',usuario_apellido='$this->apellido',usuario_correo='$this->email',
         $consulta = "update t_usuario set dni_user='$this->dni',tipodni_user='$this->tipodocumento',nom_user='$this->nom_user',apell_user='$this->apell_user',sex_user='$this->sex_user',mail_user='$this->mail_user',tel_user='$this->tel_user',civil_user='$this->civil_user',lugarnac_user='$this->lugarnac_user',nac_user='$this->nac_user',localidad_user='$this->localidad_user',estrato_user='$this->estrato_user',barrio_user='$this->barrio_user',archivo_usuario='$this->archivo_usuario',responsable_user='$this->responsable_user',cel_respon_user='$this->cel_respon_user',foto_user='$this->foto_user',fecha_registro_user='$this->fecha_registro_user',carrera_user='$this->carrera_user',universidad_user='$this->universidad_user',id_colegio='$this->id_colegio',id_estado='$this->id_estado',id_jornada='$this->id_jornada',rol_user='$this->rol_user',id_materia_user='$this->id_materia_user' where(dni_user='$this->dni')";
         if (mysql_query($consulta)) {
-            echo"<script language='javascript'> alert('La Actualizacion es un EXITO');  </script>";
+            echo"<script language='javascript'> alert('Actualizacion Exitosa');  </script>";
 
             echo"<script language='javascript'>location.href=\"../vistas/co/verDatosColaborador.php\"   </script>";
         } else {
@@ -367,7 +367,7 @@ class usuario {
         //                                         '$this->universidad_user','$this->id_colegio','$this->id_estado','$this->id_jornada','$this->rol_user','$this->id_materia_user')";  
 
         if (mysql_query($consulta)) {
-            echo"<script language='javascript'> alert('El registro es un EXITO');  </script>";
+            echo"<script language='javascript'> alert('Registro Exitoso');  </script>";
             echo"<script language='javascript'>location.href=\"../vistas/administrador.php\"   </script>";
         } else {
 
@@ -382,7 +382,7 @@ class usuario {
         $consulta = "INSERT into t_usuario(dni_user,tipodni_user,nom_user,apell_user,clave_user,sex_user,mail_user,tel_user,civil_user,lugarnac_user,nac_user,localidad_user,estrato_user,barrio_user,archivo_usuario,responsable_user,cel_respon_user,foto_user,fecha_registro_user,carrera_user,universidad_user,id_colegio,id_estado,id_jornada,rol_user,id_materia_user) values('$this->dni','$this->tipodocumento','$this->nom_user','$this->apell_user','$this->clave_user','$this->sex_user','$this->mail_user','$this->tel_user','$this->civil_user','$this->lugarnac_user','$this->nac_user','$this->localidad_user','$this->estrato_user','$this->barrio_user','$this->archivo_usuario','$this->responsable_user','$this->cel_respon_user','$this->foto_user','$this->fecha_registro_user','$this->carrera_user','$this->universidad_user','$this->id_colegio','$this->id_estado','$this->id_jornada','$this->rol_user','$this->id_materia_user')";
 
         if (mysql_query($consulta)) {
-            echo"<script language='javascript'> alert('El registro es un EXITO');  </script>";
+            echo"<script language='javascript'> alert('Registro Exitoso');  </script>";
             echo"<script language='javascript'>location.href=\"../vistas/ListarColaborador.php\"   </script>";
         } else {
             echo " " . mysql_error();
@@ -392,7 +392,7 @@ class usuario {
     public function desactivar() {
         $consulta = " update t_usuario set id_estado = '2' WHERE dni_user='$this->dni'";
         if (mysql_query($consulta)) {
-            echo"<script language='javascript'> alert('La Desactivacion es un EXITO');</script>";
+            echo"<script language='javascript'> alert(' Desactivacion es un Exito');</script>";
             echo"<script language='javascript'>location.href=\"../vistas/listarAspirante.php\"</script>";
         } else {
             echo"<script language='javascript'> alert('ERROR, No se Desactivo'); </script>";
@@ -403,7 +403,7 @@ class usuario {
     public function Activar() {
         $consulta = " update t_usuario set id_estado = '1' WHERE dni_user='$this->dni'";
         if (mysql_query($consulta)) {
-            echo"<script language='javascript'> alert('La Activacion es un EXITO');</script>";
+            echo"<script language='javascript'> alert(' Activacion exitosa');</script>";
             echo"<script language='javascript'>location.href=\"../vistas/listarAspirante.php\"</script>";
         } else {
             echo"<script language='javascript'> alert('ERROR, No se Desactivo'); </script>";
