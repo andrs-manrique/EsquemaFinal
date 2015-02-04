424f7c58dcf229f7090771d912ac2801b83b75da
medina1991 <manumedina19@hotmail.com>
2015-02-04 10:31:45 -0500

    Update M_material.php

--- EsquemaFinal/3modelo/M_material.php
+++ EsquemaFinal/3modelo/M_material.php
@@ -33,7 +33,7 @@ public function agregarMaterial() {
 $consulta ="INSERT INTO t_material(id_material,fecha_material, Titulo_material, Contenido,area_material,tipo_material, url_material, autor_material,material) values ('$this->id_material','$this->fechamaterial','$this->titulomaterial','$this->contenido','$this->area','$this->tipomaterial','$this->url','$this->autor','$this->taller')";
         if (mysql_query($consulta)) {
             
-            echo"<script language='javascript'> alert('El registro es un EXITO ');  </script>";
+            echo"<script language='javascript'> alert('Registro Exitoso ');  </script>";
             echo"<script language='javascript'>location.href=\"../vistas/co/listaMaterial.php\"   </script>";
         } else {
              echo mysql_error(); 
@@ -45,7 +45,7 @@ $consulta ="INSERT INTO t_material(id_material,fecha_material, Titulo_material, 
     public function actualizarMaterial() { //                                                                                                                                                                                                                                      usuario_nombre='$this->nombre',usuario_apellido='$this->apellido',usuario_correo='$this->email',
         $consulta = "update t_material set id_material='$this->id_material',Titulo_material='$this->titulomaterial',Contenido='$this->contenido',area_material='$this->area',tipo_material='$this->tipomaterial',url_material='$this->url',autor_material='$this->autor' where(id_material='$this->id_material')";
        if (mysql_query($consulta)) {
-            echo"<script language='javascript'> alert('La Actualizacion Material es un EXITO');  </script>";
+            echo"<script language='javascript'> alert(' Actualizacion Material Exitosa');  </script>";
               
                 echo"<script language='javascript'>location.href=\"../vistas/Material.php\"   </script>";
        }else{
